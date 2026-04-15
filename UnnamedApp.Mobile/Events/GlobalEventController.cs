9ff03d63e3190ef7cf7a1716cc616d54bc586527
Vinicius Santos <vinixreduzino@gmail.com>
2026-04-15 15:49:55 -0300

    Interface básica com o popup de sort da tela comprados

--- UnnamedApp.Mobile/Events/GlobalEventController.cs
+++ UnnamedApp.Mobile/Events/GlobalEventController.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UnnamedApp.SharedKernel.Data;
 
 namespace UnnamedApp.Mobile.Events
 {
     public class GlobalEventController
     {
+        public  List<Item>? GlobalItemList { get; set; }
+
     }
 }
