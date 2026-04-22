921df38c8fed9ba0c24f6b114988f2fcbaf944f3
Vinicius Santos <vinixreduzino@gmail.com>
2026-04-22 15:06:04 -0300

    Criação de metodos auxiliares para acesso ao banco de dados

--- UnnamedApp.Mobile/Services/DatabaseService.cs
+++ UnnamedApp.Mobile/Services/DatabaseService.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using Android.Graphics.Pdf.Models;
+using Microsoft.EntityFrameworkCore;
+
 using UnnamedApp.SharedKernel.Data;
 
 namespace UnnamedApp.Mobile.Services
@@ -14,11 +15,51 @@ namespace UnnamedApp.Mobile.Services
             _dbContext = applicationContext;
         }
 
+        public async Task<List<ItemList>> GetAllItemLists()
+        {
+            return await _dbContext.ItemLists.ToListAsync();
+        }
+        public async Task<ItemList> GetItemList(Guid id)
+        {
+            return await _dbContext.ItemLists.FirstAsync(x => x.Id == id);
+        }   
+
+        public async Task EditItemList(ItemList itemList)
+        {
+            _dbContext.ItemLists.Update(itemList);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteItemList(Guid Id)
+        {
+            await _dbContext.ItemLists.Where(x=>x.Id == Id).ExecuteDeleteAsync();
+        }
+        public async Task<List<Item>> GetItemsFromList(Guid listId)
+        {
+            return await _dbContext.Items.Where(x => x.ItemListId == listId).ToListAsync();
+        }
+        public async Task<List<Item>> GetAllItems()
+        {
+            return await _dbContext.Items.ToListAsync();
+        }
+        public async Task<Item> GetItem(Guid id)
+        {
+            return await _dbContext.Items.FirstAsync(x => x.Id == id);
+        }
         public async Task SaveItem(Item item)
         {
             await _dbContext.Items.AddAsync(item);
         }
+        public async Task EditItem(Item item)
+        {
+            _dbContext.Items.Update(item);
+            await _dbContext.SaveChangesAsync();
+        }
 
-
+        public async Task DeleteItem(Item item)
+        {
+            await _dbContext.Items.Where(x => x.Id == item.Id).ExecuteDeleteAsync();
+        }   
+         
     }
 }
